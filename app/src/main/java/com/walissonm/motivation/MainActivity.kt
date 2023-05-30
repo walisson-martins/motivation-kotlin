a9790c9b07a253ed4d266bfefa12c0e7bf6a5c01
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-30 12:08:11 -0300

    feat: configurando projeto e adicionando telas

--- app/src/main/java/com/walissonm/motivation/MainActivity.kt
+++ app/src/main/java/com/walissonm/motivation/MainActivity.kt
@@ -2,10 +2,26 @@ package com.walissonm.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.walissonm.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.button_new_phrase) {
+            var s = ""
+        }
     }
 }
---
+++ app/src/main/java/com/walissonm/motivation/UserActivity.kt
@@ -0,0 +1,39 @@
+package com.walissonm.motivation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.walissonm.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityUserBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+
+        supportActionBar?.hide()
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+
+        if (name != "") {
+            //navegação de Activity(
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
